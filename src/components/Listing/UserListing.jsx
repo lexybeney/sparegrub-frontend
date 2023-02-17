b9478b649ffe54dca8662dd7cecce60481fdc4a9
Your Name <you@example.com>
2023-02-17 15:11:37 +0000

    userlisting now pulls only from local store

--- src/components/Listing/UserListing.jsx
+++ src/components/Listing/UserListing.jsx
@@ -1,45 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import ListingItem from "./ListingItem";
 import { Accordion } from "react-bootstrap";
-import { apiUrl } from "../../sparegrubApi/apiUrl";
-import axios from "axios";
-import { getUserListing } from "../../sparegrubApi";
-import { SET_USER_LISTING } from "../../redux/types";
 
 const UserListing = () => {
-  // const userListing = useSelector((state) => state.userListing);
-  const token = useSelector((state) => state.token);
-  let [listing, setListing] = useState(null);
-  const dispatch = useDispatch();
+  const userListing = useSelector((state) => state.userListing.items);
 
-  useEffect(() => {
-    // async function getListing() {
-    //   const result = await axios.get(`${apiUrl}/read/listing`, {
-    //     headers: { token },
-    //   });
-    //   if (result.data.status === 1) {
-    //     setListing(result.data.results);
-    //   }
-    // }
-    // getListing();
-
-    async function getListing() {
-      const userListing = await getUserListing(token);
-      if (userListing === "No items listed for this user") {
-        return;
-      }
-      setListing(userListing);
-      dispatch({ type: SET_USER_LISTING, payload: userListing });
-    }
-    getListing();
-  }, []);
-
-  if (listing !== null) {
+  if (userListing !== "No items listed for this user") {
     return (
       <>
         <Accordion>
-          {listing.map((item) => {
+          {userListing.map((item) => {
             return <ListingItem key={item.item_id} item={item} />;
           })}
         </Accordion>
