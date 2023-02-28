da0fdd2d8584107665faa467a40b6fb334961087
Your Name <you@example.com>
2023-02-28 10:43:42 +0000

    remove button for basket items sends to DB and updates store

--- src/components/Basket/BasketItem.jsx
+++ src/components/Basket/BasketItem.jsx
@@ -1,16 +1,23 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { REMOVE_FROM_BASKET } from "../../redux/types";
 import { Card, Button } from "react-bootstrap";
 import locationIcon from "../../assets/images/icons/location_blue.svg";
+import { apiUrl } from "../../sparegrubApi/apiUrl";
+import axios from "axios";
 
 const BasketItem = (props) => {
+  const [removing, setRemoving] = useState(false);
+  const [error, setError] = useState(false);
+  const token = useSelector((state) => state.token);
+
   let {
     item_name,
     quantity,
     collection_location,
     extra_details,
     collection_details,
+    item_id,
   } = props.item;
   const dispatch = useDispatch();
 
@@ -22,8 +29,19 @@ const BasketItem = (props) => {
     collection_details = "N/A";
   }
 
-  const remove = () => {
-    dispatch({ type: REMOVE_FROM_BASKET, payload: props.item });
+  const remove = async () => {
+    setRemoving(true);
+    const result = await axios.put(
+      `${apiUrl}/update/item`,
+      { status: "removed", id: item_id },
+      { headers: { token } }
+    );
+    console.log(result);
+    if (result.data.status === 1) {
+      dispatch({ type: REMOVE_FROM_BASKET, payload: props.item });
+    } else {
+      setError(true);
+    }
   };
 
   return (
@@ -42,7 +60,11 @@ const BasketItem = (props) => {
           remove();
         }}
       >
-        Remove from basket
+        {error
+          ? "Error removing from basket"
+          : removing
+          ? "Removing..."
+          : "Remove from basket"}
       </Button>
       <Button>Checkout item</Button>
     </Card>
