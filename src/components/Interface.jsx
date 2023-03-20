20dd23f0e7f9807e5c6a5f3fe92cc1e94c0b79b8
Your Name <you@example.com>
2023-03-20 15:02:34 +0000

    user listing, basket and profile retrieved by polling every 5 minutes

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Home from "./Home/Index";
 import Signup from "./Signup/Index";
 import Basket from "./Basket/Index";
@@ -10,9 +10,56 @@ import Login from "./Login/Index";
 import logo from "../assets/images/logos/logo_dark_bg.svg";
 import { Button } from "react-bootstrap";
 import CollectionConfirmationWindow from "./CollectionConfirmationWindow/CollectionConfirmationWindow";
+import {
+  EDIT_PROFILE,
+  SET_USER_LISTING,
+  SET_LISTING_COLLECTION,
+  REPLACE_BASKET,
+} from "../redux/types";
+import {
+  getUserData,
+  getUserListing,
+  getUserBasket,
+} from "../sparegrubApi/index";
 
 const Interface = () => {
   const screenMode = useSelector((state) => state.screenMode);
+  const token = useSelector((state) => state.token);
+  const dispatch = useDispatch();
+
+  const syncData = async () => {
+    const user = await getUserData(token);
+    dispatch({ type: EDIT_PROFILE, payload: user });
+    const userListing = await getUserListing(token);
+    if (userListing === "No items listed for this user") {
+      dispatch({
+        type: SET_USER_LISTING,
+        payload: [],
+      });
+      dispatch({
+        type: SET_LISTING_COLLECTION,
+        payload: [],
+      });
+    } else {
+      dispatch({
+        type: SET_USER_LISTING,
+        payload: userListing.availableItems,
+      });
+      dispatch({
+        type: SET_LISTING_COLLECTION,
+        payload: userListing.itemsToBeCollected,
+      });
+    }
+    const user_id = user.id;
+    const basket = await getUserBasket(token, user_id);
+    dispatch({ type: REPLACE_BASKET, payload: basket });
+  };
+
+  useEffect(() => {
+    setInterval(() => {
+      syncData();
+    }, 300000);
+  }, []);
 
   return (
     <>
